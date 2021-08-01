a262a3e24767acce8dd4febd980e3032173940a1
TSrgy <20205309+TSrgy@users.noreply.github.com>
2021-08-01 18:07:03 +0500

    Create solution

---
+++ src/EasyHash/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasyHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
